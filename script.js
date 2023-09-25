69034e7cc89ae985d7bc760a7f0d5c5db1bc30ad
Guilherme Sartori <guilherme.sartori@orys.com.br>
2023-09-25 10:34:42 -0300

    add buttons

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const openWhatsapp = () => {
-    window.open("https://wa.link/gt8jst", "_blank")
-}
-
-const sendMail = () => {
-    window.open("mailto:guilhermekodenvis@gmail.com", "_blank")
+const openLink = (link) => {
+    window.open(link, "_blank")
 }
